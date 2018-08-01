1a1be0aa0a4a03d938497257587d474cd025060b
Gavin Tranter <gavin@yahoo.co.uk>
2018-08-01 22:48:16 +0100

    Use parameterized tests

---
+++ src/test/java/uk/trantr/kata/marsroverj/LocationConverter.java
@@ -0,0 +1,27 @@
+package uk.trantr.kata.marsroverj;
+
+import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.params.converter.ArgumentConversionException;
+import org.junit.jupiter.params.converter.ArgumentConverter;
+
+public class LocationConverter implements ArgumentConverter {
+    @Override
+    public Object convert(Object source, ParameterContext context) throws ArgumentConversionException {
+        if (source instanceof Location) {
+            return source;
+        }
+
+        if (source instanceof String) {
+            String location = (String) source;
+            String[] split = location.split("/");
+
+            int x = Integer.parseInt(split[0]);
+            int y = Integer.parseInt(split[1]);
+            Heading heading = Heading.valueOf(split[2]);
+
+            return new Location(x, y, heading);
+        }
+
+        throw new ArgumentConversionException(source + " is not a valid location");
+    }
+}
--- src/test/java/uk/trantr/kata/marsroverj/RoverTest.java
+++ src/test/java/uk/trantr/kata/marsroverj/RoverTest.java
@@ -1,6 +1,8 @@
 package uk.trantr.kata.marsroverj;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.converter.ConvertWith;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static uk.trantr.kata.marsroverj.Heading.NORTH;
@@ -9,30 +11,16 @@ class RoverTest {
 
     private static final Location INITIAL_LOCATION = new Location(1, 1, NORTH);
 
-    @Test
-    void willMoveRoverForwards() {
-        Rover rover = new Rover(INITIAL_LOCATION);
+    private final Rover rover = new Rover(INITIAL_LOCATION);
 
-        rover.process("f");
+    @ParameterizedTest(name = "Command: {0} will cause Rover to be at {1}")
+    @CsvSource({"f, 1/2/NORTH",
+    "b, 1/0/NORTH",
+    "a, 1/1/NORTH"})
+    void willMoveRover(String commandSequence, @ConvertWith(LocationConverter.class) Location finalLocation) {
+        rover.process(commandSequence);
 
-        assertThat(rover).isEqualTo(new Rover(new Location(1, 2, NORTH)));
+        assertThat(rover).isEqualTo(new Rover(finalLocation));
     }
 
-    @Test
-    void willMoveRoverBackwards() {
-        Rover rover = new Rover(INITIAL_LOCATION);
-
-        rover.process("b");
-
-        assertThat(rover).isEqualTo(new Rover(new Location(1, 0, NORTH)));
-    }
-
-    @Test
-    void willIgnoreUnknownCommands() {
-        Rover rover = new Rover(INITIAL_LOCATION);
-
-        rover.process("a");
-
-        assertThat(rover).isEqualTo(new Rover(INITIAL_LOCATION));
-    }
 }
