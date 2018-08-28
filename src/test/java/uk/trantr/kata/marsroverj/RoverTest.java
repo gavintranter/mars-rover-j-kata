111b5f77e93d233a7e2aef197e3305afb3bec392
Gavin Tranter <gavin@yahoo.co.uk>
2018-08-28 17:03:45 +0100

    Add journey tests

--- src/test/java/uk/trantr/kata/marsroverj/RoverTest.java
+++ src/test/java/uk/trantr/kata/marsroverj/RoverTest.java
@@ -1,5 +1,7 @@
 package uk.trantr.kata.marsroverj;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.converter.ConvertWith;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -13,11 +15,17 @@ class RoverTest {
 
     private static final Location INITIAL_LOCATION = new Location(1, 1, NORTH);
 
-    private final Rover rover = new Rover(INITIAL_LOCATION);
+    Rover rover;
+
+    @BeforeEach
+    void setUp() {
+        rover = new Rover(INITIAL_LOCATION);
+    }
+
 
     @ParameterizedTest(name = "Case: {index} -> Command: {0} will cause Rover to be at {1}")
     @CsvSource({"ff, 1/3/NORTH",
-    "bb, 1/-1/NORTH"})
+                "bb, 1/-1/NORTH"})
     void willMoveRover(String commandSequence,
                        @ConvertWith(LocationConverter.class) Location finalLocation) {
 
@@ -26,4 +34,21 @@ class RoverTest {
         assertThat(rover).isEqualTo(new Rover(finalLocation));
     }
 
+    @ParameterizedTest(name = "Case: {index} -> Command: {0} will cause Rover to be at {1}")
+    @CsvSource({"ffrffrffrffr",
+                "fflfflfflffl",
+                "bbrbbrbbrbbr",
+                "bblbblbblbbl"})
+    void roverTravelInASquareShapeReturningToItsOrigin(String commandSequence) {
+        rover.process(commandSequence);
+
+        assertThat(rover).isEqualTo(new Rover(INITIAL_LOCATION));
+    }
+
+    @Test
+    void willGoOnMoreComplexJourneyReturningToOrigin() {
+        rover.process("frflfrflfrfrffbrflffrffr");
+
+        assertThat(rover).isEqualTo(new Rover(INITIAL_LOCATION));
+    }
 }
