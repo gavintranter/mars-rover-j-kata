f643397491096a45f88c3fc91d2289cd45a7a4fc
Gavin Tranter <gavin@yahoo.co.uk>
2018-08-28 09:57:09 +0100

    Improve parameter order

--- src/test/java/uk/trantr/kata/marsroverj/navigation/CoordinateTest.java
+++ src/test/java/uk/trantr/kata/marsroverj/navigation/CoordinateTest.java
@@ -9,12 +9,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 class CoordinateTest {
 
     @ParameterizedTest(name = "Case: {index} -> Vector {0} Will transform {1} to {2}")
-    @CsvSource({"0/1, 1/1, 1/2",
-            "0/-1, 1/1, 1/0",
-            "1/0, 1/1, 2/1",
-            "-1/0, 1/1, 0/1"})
-    void willAddCoordinates(@ConvertWith(CoordinateConverter.class) Coordinate newCoordinate,
-                            @ConvertWith(CoordinateConverter.class) Coordinate initial,
+    @CsvSource({"1/1, 0/1, 1/2",
+                "1/1, 0/-1, 1/0",
+                "1/1, 1/0, 2/1",
+                "1/1, -1/0, 0/1"})
+    void willAddCoordinates(@ConvertWith(CoordinateConverter.class) Coordinate initial,
+                            @ConvertWith(CoordinateConverter.class) Coordinate newCoordinate,
                             @ConvertWith(CoordinateConverter.class) Coordinate result) {
 
         Coordinate actual = initial.add(newCoordinate);
@@ -24,10 +24,10 @@ class CoordinateTest {
 
     @ParameterizedTest(name = "Case: {index} -> Vector {0} -> {1}")
     @CsvSource({"0/0, 0/0",
-            "0/1, 0/-1",
-            "1/0, -1/0",
-            "12/0, -12/0",
-            "6/7, -6/-7"})
+                "0/1, 0/-1",
+                "1/0, -1/0",
+                "12/0, -12/0",
+                "6/7, -6/-7"})
     void willInvertCoordinate(@ConvertWith(CoordinateConverter.class) Coordinate initial,
                               @ConvertWith(CoordinateConverter.class) Coordinate result) {
 
