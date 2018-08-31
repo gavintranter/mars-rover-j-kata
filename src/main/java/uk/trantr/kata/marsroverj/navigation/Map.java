a9fd58c7633bbf171af6e260c3f61ddd1c4193e9
Gavin Tranter <gavin@yahoo.co.uk>
2018-08-31 12:20:07 +0100

    Check new coordinate for danger

--- src/main/java/uk/trantr/kata/marsroverj/navigation/Map.java
+++ src/main/java/uk/trantr/kata/marsroverj/navigation/Map.java
@@ -1,13 +1,15 @@
 package uk.trantr.kata.marsroverj.navigation;
 
+import java.util.Set;
 import java.util.function.IntFunction;
 
 public class Map {
 
     private final IntFunction<Integer> latitudeAdjustment;
     private final IntFunction<Integer> longitudeAdjustment;
+    private final Set<Coordinate> obstacles;
 
-    public Map(int latitudeExtent, int longitudeExtent) {
+    public Map(int latitudeExtent, int longitudeExtent, Set<Coordinate> obstacles) {
         latitudeAdjustment = x -> {
             int beyondExtent = x % latitudeExtent;
             return (beyondExtent == 0) ? latitudeExtent : beyondExtent;
@@ -17,9 +19,15 @@ public class Map {
             int beyondExtent = x % longitudeExtent;
             return (beyondExtent == 0) ? longitudeExtent : beyondExtent;
         };
+
+        this.obstacles = obstacles;
     }
 
     public Coordinate moveTo(Coordinate newNorthernCoordinate) {
         return newNorthernCoordinate.adjustBy(latitudeAdjustment, longitudeAdjustment);
+    }
+
+    public boolean isSafe(Coordinate coordinate) {
+        return !(obstacles.contains(coordinate));
     }
 }
--- src/test/java/uk/trantr/kata/marsroverj/navigation/MapTest.java
+++ src/test/java/uk/trantr/kata/marsroverj/navigation/MapTest.java
@@ -1,14 +1,19 @@
 package uk.trantr.kata.marsroverj.navigation;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.converter.ConvertWith;
 import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class MapTest {
 
-    private final Map map = new Map(5, 4);
+    private final Set<Coordinate> obstacles = new HashSet<>();
+    private final Map map = new Map(5, 4, obstacles);
 
     @ParameterizedTest(name = "Case: {index} -> Will adjust {0} to {1}")
     @CsvSource({"1/5, 1/1",
@@ -23,5 +28,24 @@ class MapTest {
         Coordinate adjustedCoordinate = map.moveTo(newTopCoordinate);
 
         assertThat(adjustedCoordinate).isEqualTo(expectedBottomCoordinate);
+    }
+
+    @Test
+    void willReportCoordinateIsFreeOfDanger() {
+        Coordinate coordinate = new Coordinate(1, 1);
+
+        boolean result = map.isSafe(coordinate);
+
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void willReportCoordinateIsNotFreeOfDanger() {
+        obstacles.add(new Coordinate(1, 1));
+        Coordinate coordinate = new Coordinate(1, 1);
+
+        boolean result = map.isSafe(coordinate);
+
+        assertThat(result).isFalse();
     }
 }
