84bd639c944a5d10f15be18131c0fd8bac9a040e
Gavin Tranter <gavintranter@users.noreply.github.com>
2020-05-13 15:27:09 +0100

    Reduce access scope

--- src/main/java/uk/trantr/kata/marsroverj/navigation/Heading.java
+++ src/main/java/uk/trantr/kata/marsroverj/navigation/Heading.java
@@ -5,79 +5,79 @@ import java.util.function.IntFunction;
 public enum Heading {
     NORTH {
         @Override
-        public Coordinate getVector() {
+        Coordinate getVector() {
             return P_Y_VECTOR;
         }
 
         @Override
-        public Heading clockwise() {
+        Heading clockwise() {
             return EAST;
         }
 
         @Override
-        public Heading anticlockwise() {
+        Heading anticlockwise() {
             return WEST;
         }
     },
     EAST {
         @Override
-        public Coordinate getVector() {
+        Coordinate getVector() {
             return P_X_VECTOR;
         }
 
         @Override
-        public Heading clockwise() {
+        Heading clockwise() {
             return SOUTH;
         }
 
         @Override
-        public Heading anticlockwise() {
+        Heading anticlockwise() {
             return NORTH;
         }
     },
     SOUTH {
         @Override
-        public Coordinate getVector() {
+        Coordinate getVector() {
             return N_Y_VECTOR;
         }
 
         @Override
-        public Heading clockwise() {
+        Heading clockwise() {
             return WEST;
         }
 
         @Override
-        public Heading anticlockwise() {
+        Heading anticlockwise() {
             return EAST;
         }
     },
     WEST {
         @Override
-        public Coordinate getVector() {
+        Coordinate getVector() {
             return N_X_VECTOR;
         }
 
         @Override
-        public Heading clockwise() {
+        Heading clockwise() {
             return NORTH;
         }
 
         @Override
-        public Heading anticlockwise() {
+        Heading anticlockwise() {
             return SOUTH;
         }
     };
 
-    public static final IntFunction<Integer> INVERT_FUNCTION = v -> v * -1;
+    static final IntFunction<Integer> INVERT_FUNCTION = v -> v * -1;
 
     private static final Coordinate P_X_VECTOR = Coordinate.ONE_ZERO;
     private static final Coordinate N_X_VECTOR = P_X_VECTOR.adjustBy(INVERT_FUNCTION, INVERT_FUNCTION);
     private static final Coordinate P_Y_VECTOR = Coordinate.ZERO_ONE;
     private static final Coordinate N_Y_VECTOR = P_Y_VECTOR.adjustBy(INVERT_FUNCTION, INVERT_FUNCTION);
 
-    public abstract Coordinate getVector();
+    abstract Coordinate getVector();
 
-    public abstract Heading clockwise();
+    abstract Heading clockwise();
 
-    public abstract Heading anticlockwise();
+    abstract Heading anticlockwise();
 }
